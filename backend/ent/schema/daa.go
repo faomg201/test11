3b91c3566100bcff2e2c5fc691816ec5457b8688
faomg201 <69358628+faomg201@users.noreply.github.com>
2020-12-27 02:32:15 +0700

    สร้าง -close #2

---
+++ backend/ent/schema/daa.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Daa holds the schema definition for the Daa entity.
+type Daa struct {
+	ent.Schema
+}
+
+// Fields of the Daa.
+func (Daa) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Daa.
+func (Daa) Edges() []ent.Edge {
+	return nil
+}
---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.cofaomg201/app
+
+go 1.14
+
+require github.com/facebookincubator/ent v0.2.7
